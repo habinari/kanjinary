7aac4eb6d9f02565ff8c6564e9f8c96a9d600f5d
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-12 22:46:07 +0200

    Add kanji id

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,19 @@
 import json
+import io
 
-all_kanji = []
+all_kanjis = []
 
+# Load all data
 with open('./data/all.json') as all_data:  
     data = json.load(all_data)
     for kanji in data:
-        print('kanji: ' + kanji['kanji'])
-        print('Website: ' + kanji['kunyomi'][0])
-        print('From: ' + kanji['onyomi'][0])
+        all_kanjis.append(kanji)
+
+# Save all kanji as individual json file
+index = 0
+for kanji in all_kanjis:
+    with io.open('./docs/static/kanji/' + kanji['kanji'] + '.json', 'w', encoding='utf8') as outfile:  
+        kanji['id'] = index
+        index += 1
+        json.dump(kanji, outfile, ensure_ascii=False)
+
