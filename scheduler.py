c739ebd065038bc547d522a2f6ff5b8a028dc422
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-18 21:34:56 +0200

    Try web

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,15 @@
 import json
 import io
+import os
 
 all_kanjis = []
+
+# Create static directory if not exist
+directory = './docs/static/'
+try:
+    os.stat(directory)
+except:
+    os.mkdir(directory)
 
 # Load all data
 with open('./static/all.json') as all_data:  
@@ -9,10 +17,18 @@ with open('./static/all.json') as all_data:
     for kanji in data:
         all_kanjis.append(kanji)
 
+
+# Create a directory for kanjis if not exists
+kanji_directory = './docs/static/kanji/'
+try:
+    os.stat(kanji_directory)
+except:
+    os.mkdir(kanji_directory)
+
 # Save all kanji as individual json file
 index = 0
 for kanji in all_kanjis:
-    with io.open('./docs/kanji/' + kanji['kanji'] + '.json', 'w', encoding='utf8') as outfile:
+    with io.open(kanji_directory + kanji['kanji'] + '.json', 'w', encoding='utf8') as outfile:
         kanji['id'] = index
         index += 1
         json.dump(kanji, outfile, indent=4, ensure_ascii=False)
@@ -32,10 +48,17 @@ for kanji in all_kanjis:
         level['kanjis'].append(kanji)
         all_levels.append(level)
 
+# Create a directory for levels if not exists
+lvl_directory = './docs/static/lvl/'
+try:
+    os.stat(lvl_directory)
+except:
+    os.mkdir(lvl_directory)
+
 # Write all levels
 index = 0
 for level in all_levels:
-    with io.open('./docs/lvl/' + level['name'] + '.json', 'w', encoding='utf8') as outfile:
+    with io.open(lvl_directory + level['name'] + '.json', 'w', encoding='utf8') as outfile:
         level['id'] = index
         index += 1
         json.dump(level, outfile, indent=4, ensure_ascii=False)
