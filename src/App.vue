6b5ca7f6edd28dd40be624e76d2a3bdaef01bbe7
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-19 18:11:38 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -52,12 +52,20 @@
 
     <router-view/>
 
+    <Footer/>
   </div>
 </template>
 
 <script>
+
+import Footer from '@/components/Footer.vue'
+
 export default {
   name: 'app',
+
+  components: {
+    Footer
+  },
 
   data: function() {
     return {
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="footer m-t-xxl has-background-info">
+      <div class="content has-text-centered has-text-white">
+        <p>
+          <strong class="has-text-white">Kanjinary</strong> by <u><a class="has-text-white" href="https://www.kabi404.dev">kabi404</a></u>. The source code is licensed under
+            <u><a class="has-text-white" href="http://opensource.org/licenses/mit-license.php">MIT</a></u>.
+        </p>
+      </div>
+    </footer>
+</template>
