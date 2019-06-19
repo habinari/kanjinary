f52c60db55b7a81fa70b00d1b2aa70c11baecd71
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-19 13:52:53 +0200

    Add icon-card component

---
+++ src/components/IconCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        <div class="card-image has-text-centered">
+            <i class=""></i>
+        </div>
+        <div class="card-content">
+            <div class="content">
+                <h4>{{title}}</h4>
+                <p>{{content}}</p>
+                <p><a href="#">{{link}}</a></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'icon-card',
+
+  data: function() {
+    return {
+      test: 'Nav icon-card!',
+      title: 'title',
+      content: 'content',
+      link: 'link'
+    }
+  },
+
+  computed: {
+    current: function() {
+      return this.$route.name
+    }
+  }
+
+}
+</script>
+
