9e6c78dc77cbe075c5bfd3258c1a66aaaadfeeab
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-19 12:48:14 +0200

    main routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
+
+// Main libraries imports
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+// View imports
+import Home from './views/Home.vue'
+import Kanji from './views/Kanji.vue'
+import Level from './views/Level.vue'
+
 
 export default new Router({
   routes: [
@@ -12,12 +19,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/kanji',
+      name: 'kanji',
+      component: Kanji
+    },
+    {
+      path: '/lvl',
+      name: 'level',
+      component: Level
     }
   ]
 })
