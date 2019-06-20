a6d123dfac25ceb1c8e4781ff657b4141d5e3bd0
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-20 19:10:29 +0200

    Add kanji details

--- src/components/Kanji.vue
+++ src/components/Kanji.vue
@@ -5,6 +5,41 @@
                 <h1 class="title is-size-1">{{kanji.kanji}}</h1>
             </div>
         </div>
+
+        <div class="columns">
+            <div class="is-offset-2 column is-3 has-text-right">
+                <p class="is-size-5">Kunyomi:</p>
+            </div>
+            <div class="column is-4 has-text-left">
+                <p class="is-size-5">
+                    <span v-for="yomi in kanji.kunyomi" :key="yomi" class="m-l-md">{{yomi}}</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="is-offset-2 column is-3 has-text-right">
+                <p class="is-size-5">Onyomi:</p>
+            </div>
+            <div class="column is-4 has-text-left">
+                <p class="is-size-5">
+                    <span v-for="yomi in kanji.onyomi" :key="yomi" class="m-l-md">{{yomi}}</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="columns is-centered m-t-md">
+            <div class="column is-2">
+                <p>Radical:</p> {{kanji.radicals[0]}}
+            </div>
+            <div class="column is-2">
+                <p>Trazos:</p> {{kanji.strokes}}
+            </div>
+            <div class="column is-2">
+                <p>Nivel:</p> {{kanji.lvl}}
+            </div>
+        </div>
+
     </section>
 </template>
 
