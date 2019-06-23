994902affe2891bb5e35830db06c948148d3fdfd
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-23 17:54:19 +0200

    Add empty classes

---
+++ src/models/kanji.js
@@ -0,0 +1,21 @@
+class Kanji {
+    constructor(
+        ideogram,
+        kunyomi,
+        onyomi,
+        words,
+        parts,
+        radical,
+        strokes,
+        lvl
+    ) {
+        this.ideogram = ideogram
+        this.kunyomi = kunyomi
+        this.onyomi = onyomi
+        this.words = words
+        this.parts = parts
+        this.radical = radical
+        this.strokes = strokes
+        this.lvl = lvl
+    }
+}
