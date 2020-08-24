206a5e2d401b2561f968487357724ea6355a8e88
Javier Parada <javierparada73@outlook.com>
2020-08-24 17:55:07 +0200

    Add database init script

---
+++ scripts/db_init.py
@@ -0,0 +1,50 @@
+import json
+from pymongo import MongoClient 
+
+try: 
+    conn = MongoClient() 
+    print("MongoDB Connection success.") 
+except:   
+    print("Could not connect to MongoDB") 
+
+mongodb = conn.kanjinary
+
+with open('./static/all.json', 'r') as read_file:
+    data = json.load(read_file)
+
+# --- save all kanjis ---
+mongodb['kanjis'].drop()
+for kanji_data in data:
+    mongodb.kanjis.insert_one(kanji_data)
+    
+# --- save kanjis by radicals ---
+mongodb['radicals'].drop()
+radicals = {}
+for kanji_data in data:
+    radical = kanji_data['radical']
+    if not radical in radicals:
+        radicals[radical] = []
+    radicals[radical].append(kanji_data)
+
+for key, value in radicals.items():
+    mongodb.radicals.insert_one({
+        'ideogram': key,
+        'kanjis': value
+    })
+radicals = None
+
+# --- save kanjis by stroke numer ---
+mongodb['strokes'].drop()
+strokes = {}
+for kanji_data in data:
+    stroke_num = kanji_data['strokes']
+    if not stroke_num in strokes:
+        strokes[stroke_num] = []
+    strokes[stroke_num].append(kanji_data)
+
+for key, value in strokes.items():
+    mongodb.strokes.insert_one({
+        'strokes': key,
+        'kanjis': value
+    })
+strokes = None
